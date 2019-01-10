df5e4363747cb6bb2dc4e9b08bc358c477a5f7f0
deadpool1347 <fuurria@yandex.ru>
2019-01-10 17:41:41 +0300

    Login and registarte/

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 //var articleRouter = require('./routes/article');
@@ -13,6 +13,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+//app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require ( 'mysql2' ) ;
+var mailgun = require("mailgun-js");
+var api_key = '6a232dacbbdc434f21d4e73621d49bde-060550c6-846510c4';
+var DOMAIN = 'sandbox271a7fb6dc7141af87ae09f601348477.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
+
 
 var connection = mysql . createConnection ( {
   host: 'localhost' ,
@@ -33,7 +38,7 @@ router.get('/', function(req, res, next) {
       connection.query(
         `SELECT * FROM Article ${join} ${where}` ,
         function(err, articles) {
-          res.render('index', {title: 'KMJ School', articles: articles, query:req.query, subjects: subjects});
+          res.render('index', {title: 'KMJ School', articles: articles, query:req.query, subjects: subjects, session: req.session});
         }
       );
     }
@@ -59,28 +64,114 @@ router.get('/articles/:id', function(req, res, next) {
 });
 
 router.get('/registration', function(req, res, next) {
+  if(Object.keys(req.query).length === 0){
+    res.render('registration');
+    return;
+  }
+
+  var error;
+
+  if(req.query.password1!==req.query.password2){
+    error='Пароли не совпадают!';
+    res.render('registration', {query: req.query, error: error});
+    return;
+  }
+
   connection.query(
-    'SELECT * FROM Users',
-    function(err, articles) {
-      res.render('registration', { title: 'Регистрация', articles: articles});
+    'SELECT * FROM Users where login='+req.query.email,
+    function(err, user) {
+      if(user){
+        error='Такой login уже существует!';
+        res.render('registration', {query: req.query, error: error});
+        return;
+      }
+      var key = Math.floor(Math.random() * 1000000000) + 1;
+      connection.query(
+        'insert into User (login, password, idStatus, keyCode) values("'+req.query.email+'", '+req.query.password1+', 1, '+key+' )',
+        function(err, result) {
+          console.log(result);
+          console.log(err);
+          console.log('insert into User (login, password, idStatus) values('+req.query.email+', '+req.query.password1+', 1)');
+          var data = {
+            from: 'KMJ School <me@samples.mailgun.org>',
+            to: req.query.email,
+            subject: 'Активация аккаунта',
+          };
+          var link ='http://localhost:3000/email-activation?link='+key+'&email='+req.query.email;
+          res.render('message',{link: link}, function (err, html) {
+                if (err) {
+                    return next(err);
+                }
+                data.html = html;
+            });
+          mailgun.messages().send(data, function (error, body) {
+            res.render('registrated', {msg: 'Регистрация прошла успешно. Письмо для подтверждения мы вам отправили на вашу почту.' });
+
+          });
+        }
+      );
     }
   );
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/email-activation', function(req, res, next) {
+  console.log(req.query);
+  console.log();
   connection.query(
-    'SELECT * FROM Users',
-    function(err, users) {
-      res.render('login', { title: 'Авторизация'});
+    'SELECT * FROM User where login="'+req.query.email+'"',
+    function(err, user) {
+      console.log('SELECT * FROM User where login="'+req.query.email+'"');
+      if(user.keyCode!=req.query.key){
+          res.render('registrated', {msg: 'Ошибка! Не удалось активировать логин.'});
+          return;
+      }
+      connection.query(
+        'UPDATE User SET idStatus=2 WHERE login ="'+req.query.email+'"',
+        function(err, users) {
+          req.session.email=req.query.email;
+          res.redirect('/');
+        }
+      );
+    }
+  );
+});
+
+
+
+router.get('/login', function(req, res, next) {
+  if(Object.keys(req.query).length === 0){
+    res.render('login');
+    return;
+  }
+  var error ;
+  connection.query(
+    'SELECT * FROM User where login="'+req.query.email+'"',
+    function(err, user) {
+      console.log(user);
+      if(user.length === 0){
+        error='Такого пользователя не существует';
+        res.render('login',{error: error});
+        return;
+      }
+
+      console.log(user[0].password);
+      console.log(req.query.password);
+      if(user[0].password!=req.query.password){
+        error='Пароль не верный!';
+        res.render('login',{error: error});
+        return;
+      }
+      req.session.email=req.query.email;
+      res.redirect('/');
     }
   );
 });
 
 router.get('/user', function(req, res, next) {
   connection.query(
-    'SELECT * FROM Users',
+    'SELECT * FROM User',
     function(err, users) {
-      res.render('user', { title: 'Пользователь',user: users});
+      res.render('user', {user: users});
     }
   );
 });
