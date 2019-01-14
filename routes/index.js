e97fe9c2e5412cc2c5358d0a85caaeb9acbaf353
deadpool1347 <fuurria@yandex.ru>
2019-01-14 17:25:34 +0300

    Log in. Add article. Recovery password.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mysql = require ( 'mysql2' ) ;
 var mailgun = require("mailgun-js");
-var api_key = '6a232dacbbdc434f21d4e73621d49bde-060550c6-846510c4';
-var DOMAIN = 'sandbox271a7fb6dc7141af87ae09f601348477.mailgun.org';
+var api_key = 'key-2f09c76695a377a13554a4f01e97d874';
+var DOMAIN = 'mg.kazakovmj.ru';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
 
 
@@ -47,9 +47,26 @@ router.get('/', function(req, res, next) {
 
 router.get('/newarticles', function(req, res, next) {
   connection.query(
-    'SELECT * FROM Users',
-    function(err, users) {
-      res.render('newarticle', { title: 'Статья', user: users});
+    'SELECT * FROM Subject',
+    function(err, subjects) {
+      if(Object.keys(req.query).length === 0){
+        res.render('newarticle',{subjects: subjects, query: req.query});
+        return;
+      }
+
+      var error;
+
+      if(!req.query.theme || !req.query.name || !req.query.content){
+        error='Заполните все поля!';
+        res.render('newarticle', {query: req.query, error: error, subjects: subjects});
+        return;
+      }
+      connection.query(
+        'INSERT INTO `Article`( `name`, `content`, `idUser`, `idTheme`) VALUES ("'+req.query.name+'","'+req.query.content+'",'+req.session.idUser+','+req.query.theme+')',
+        function(err) {
+          res.redirect('/');
+        }
+      );
     }
   );
 });
@@ -89,9 +106,6 @@ router.get('/registration', function(req, res, next) {
       connection.query(
         'insert into User (login, password, idStatus, keyCode) values("'+req.query.email+'", '+req.query.password1+', 1, '+key+' )',
         function(err, result) {
-          console.log(result);
-          console.log(err);
-          console.log('insert into User (login, password, idStatus) values('+req.query.email+', '+req.query.password1+', 1)');
           var data = {
             from: 'KMJ School <me@samples.mailgun.org>',
             to: req.query.email,
@@ -115,12 +129,9 @@ router.get('/registration', function(req, res, next) {
 });
 
 router.get('/email-activation', function(req, res, next) {
-  console.log(req.query);
-  console.log();
   connection.query(
     'SELECT * FROM User where login="'+req.query.email+'"',
     function(err, user) {
-      console.log('SELECT * FROM User where login="'+req.query.email+'"');
       if(user.keyCode!=req.query.key){
           res.render('registrated', {msg: 'Ошибка! Не удалось активировать логин.'});
           return;
@@ -147,31 +158,129 @@ router.get('/login', function(req, res, next) {
   connection.query(
     'SELECT * FROM User where login="'+req.query.email+'"',
     function(err, user) {
-      console.log(user);
       if(user.length === 0){
         error='Такого пользователя не существует';
         res.render('login',{error: error});
         return;
       }
 
-      console.log(user[0].password);
-      console.log(req.query.password);
       if(user[0].password!=req.query.password){
         error='Пароль не верный!';
         res.render('login',{error: error});
         return;
       }
       req.session.email=req.query.email;
+      req.session.idUser=user[0].idUser;
+      if(user[0].idStatus==3)
+        req.session.idStatus=user[0].idStatus;
       res.redirect('/');
     }
   );
 });
 
+router.get('/logout', function(req, res, next) {
+      delete req.session.email;
+      delete req.session.idUser;
+
+      res.redirect('back');
+});
+
 router.get('/user', function(req, res, next) {
+  if(!req.session.email){
+    res.redirect('/login');
+  }
   connection.query(
-    'SELECT * FROM User',
-    function(err, users) {
-      res.render('user', {user: users});
+    'SELECT * FROM UserInfo where idUser='+req.session.idUser,
+    function(err, user) {
+
+      if (req.query && req.query.password1){
+        if(req.query.password1!==req.query.password2){
+          var error='Пароли не совпадают!';
+          res.render('/user', {user: user && user[0], error: error});
+          return;
+        }
+        connection.query(
+          'UPDATE `User` SET `password`="'+req.query.password1+'" WHERE idUser='+req.session.idUser,
+          function(err) {
+            res.render('user', {user: user && user[0]});
+          }
+        );
+        return;
+      }
+
+      if(Object.keys(req.query).length === 0){
+        res.render('user', {user: user && user[0]});
+        return;
+      }
+
+      if(user && user.length==0){
+        connection.query(
+          'INSERT INTO `UserInfo`(`idUser`, `name`, `vk`, `facebook`) VALUES ('+req.session.idUser+',"'+req.query.nickname+'","'+req.query.urlVK+'","'+req.query.urlFacebook+'")',
+          function(err, user) {
+
+            res.render('user', {user: user});
+          }
+        );
+        return;
+      }
+      connection.query(
+        'UPDATE `UserInfo` SET `name`="'+req.query.nickname+'",`vk`="'+req.query.urlVK+'",`facebook`="'+req.query.urlFacebook+'" WHERE idUser='+req.session.idUser,
+        function(err) {
+          connection.query(
+            'SELECT * FROM UserInfo where idUser='+req.session.idUser,
+            function(err, user) {
+              res.render('user', {user: user[0]});
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+router.get('/password', function(req, res, next) {
+  if(Object.keys(req.query).length === 0){
+    res.render('password');
+    return;
+  }
+console.log(req.query.email);
+  connection.query(
+    'SELECT * FROM User where login="'+req.query.email+'"',
+    function(err, user) {
+      var data = {
+        from: 'KMJ School <me@samples.mailgun.org>',
+        to: req.query.email,
+        subject: 'Восстановление пароля',
+      };
+      console.log(user);
+      var link ='http://localhost:3000/email-recovery?link='+user[0].key+'&email='+req.query.email;
+      res.render('recovery',{link: link}, function (err, html) {
+            if (err) {
+                return next(err);
+            }
+            data.html = html;
+        });
+      mailgun.messages().send(data, function (error, body) {
+          res.render('registrated', {msg: 'Восстановление пароля прошло успешно. Письмо для воостановления мы вам отправили на вашу почту.' });
+
+      });
+    }
+  );
+});
+
+router.get('/email-recovery', function(req, res, next) {
+  connection.query(
+    'SELECT * FROM User where login="'+req.query.email+'"',
+    function(err, user) {
+      if(user.keyCode!=req.query.key){
+          res.render('registrated', {msg: 'Ошибка! Не удалось восстановить праоль.'});
+          return;
+      }
+      req.session.email=req.query.email;
+      req.session.idUser=user[0].idUser;
+      if(user[0].idStatus==3)
+        req.session.idStatus=user[0].idStatus;
+      res.redirect('/user');
     }
   );
 });
